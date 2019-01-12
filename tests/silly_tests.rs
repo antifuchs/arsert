308d3678495240dbcd7179044dcffde602bb9506
Andreas Fuchs <asf@boinkor.net>
2019-01-12 17:39:38 +0100

    Tighten the ratchet on tests: check that validation ran

--- tests/silly_tests.rs
+++ tests/silly_tests.rs
@@ -7,7 +7,8 @@ use arsert_failure::{
 fn binary_ops() {
     let x = 1;
     let y = 2;
-    let validate = |ei: BinaryAssertionFailure<i32, i32>| {
+    let mut ran = false;
+    let mut validate = |ei: BinaryAssertionFailure<i32, i32>| {
         assert_eq!("x >= y", ei.expression());
         assert_eq!(vec!["x", ">=", "y"], ei.expression_parts());
 
@@ -20,29 +21,35 @@ fn binary_ops() {
             Some("2".to_string()),
             vals.get("y").map(|v| format!("{:?}", v))
         );
+        ran = true;
     };
     arsert!(#![failure_function validate] x >= y);
+    assert!(ran);
 }
 
 #[test]
 fn unary_ops() {
-    let x = true;
-    let validate = |ei: UnaryAssertionFailure<bool>| {
-        assert_eq!("!x", ei.expression());
-        assert_eq!(vec!["!", "x"], ei.expression_parts());
+    let x = Box::new(false);
+    let mut ran = false;
+    let mut validate = |ei: UnaryAssertionFailure<Box<bool>>| {
+        assert_eq!("*x", ei.expression());
+        assert_eq!(vec!["*", "x"], ei.expression_parts());
 
         let vals = ei.values();
         assert_eq!(
-            Some("true".to_string()),
+            Some("false".to_string()),
             vals.get("x").map(|v| format!("{:?}", v))
         );
+        ran = true;
     };
-    arsert!(#![failure_function validate] !x);
+    arsert!(#![failure_function validate] *x);
+    assert!(ran);
 }
 
 #[test]
 fn unclear_ops() {
-    let validate = |ei: SimpleAssertionFailure| {
+    let mut ran = false;
+    let mut validate = |ei: SimpleAssertionFailure| {
         assert_eq!("something", ei.expression());
         assert_eq!(vec!["something"], ei.expression_parts());
 
@@ -51,7 +58,9 @@ fn unclear_ops() {
             Some("false".to_string()),
             vals.get("something").map(|v| format!("{:?}", v))
         );
+        ran = true;
     };
     let something = false;
     arsert!(#![failure_function validate] something);
+    assert!(ran);
 }
